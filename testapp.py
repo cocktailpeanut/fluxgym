5ad3e711dc85bb04588a7a1897df55fcffea10b4
Mister K <678459+kairin@users.noreply.github.com>
2024-12-22 03:45:57 +0000

    Create testapp.py
    this is just to test if something is wrong with demo.launch(share=True)

---
+++ testapp.py
@@ -0,0 +1,8 @@
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
+    
+demo.launch(share=True)  # Share your demo with just 1 extra parameter 🚀
