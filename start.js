835c1bf442f7aafae139f0e663a6159674492e80
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-21 08:07:19 -0800

    proper url capture

--- start.js
+++ start.js
@@ -16,7 +16,7 @@ module.exports = {
           // The regular expression pattern to monitor.
           // When this pattern occurs in the shell terminal, the shell will return,
           // and the script will go onto the next step.
-          "event": "/http:\/\/\\S+/",   
+          "event": "/http:\/\/\\S+(?=\s|$)/m",   
 
           // "done": true will move to the next step while keeping the shell alive.
           // "kill": true will move to the next step after killing the shell.
