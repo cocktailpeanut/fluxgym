a0619a4c080603086fb93b0a996dbeffb85bd15c
6Morpheus6 <wanton-decay@gmx.net>
2026-07-28 05:47:30 +0200

    triton-windows for windows nvidia

--- torch.js
+++ torch.js
@@ -7,7 +7,10 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps"
+        "message": [
+          "uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps",
+          "uv pip install triton-windows"
+        ]
 
       }
     },
